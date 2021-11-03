bf1130f4e0701215be19ec77f7d4a9a9038f8e41
ristamaulaya09 <ristamaulaya09@gmail.com>
2021-11-03 11:17:19 +0700

    upload contoh 1, 2, 3, dan menghilangkan index php

---
+++ application/controllers/Latihan1.php
@@ -0,0 +1,21 @@
+<?php
+class Latihan1 extends CI_Controller 
+{ 
+    public function index() 
+    { 
+        echo "Selamat Datang.. selamat belajar Web Programming"; 
+        $this->load->view('view-latihan1'); 
+    } 
+    
+    public function penjumlahan($nil1, $nil2) 
+    { 
+        $this->load->model('model_latihan1'); 
+        
+        $data['nilai1'] = $nil1; 
+        $data['nilai2'] = $nil2; 
+        $data['hasil'] = $this->model_latihan1->jumlah($nil1, $nil2); 
+        
+        $this->load->view('view-latihan1', $data);
+    }
+}
+?>
